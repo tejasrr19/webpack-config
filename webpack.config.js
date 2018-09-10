716bfd22ca5b22fc03775e6b625237f2e6e259b6
Tejas <tejasrr19@gmail.com>
2018-09-10 16:43:54 -0700

    JS, JSX and Hash output file

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,32 @@ module.exports = {
     path: path.join(__dirname, "build"),
     filename: "bundle.js",
     publicPath: "/build"
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" },
+          { loader: "sass-loader" }
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Caching"
+    })
+  ]
 };
